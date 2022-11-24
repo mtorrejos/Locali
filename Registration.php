25de2da02a17e4bd20e6a7b264a48a2f7d7a51fe
mtorrejos <106376979+mtorrejos@users.noreply.github.com>
2022-11-24 18:01:48 +0800

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,3 +1,47 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['submit'])){
+
+    $fName = mysqli_real_escape_string($conn, $_POST['firstName']);
+    $lName = mysqli_real_escape_string($conn, $_POST['lastName']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pword = md5($_POST['password']);
+    $cpword = md5($_POST['cpassword']);
+    $gender = $_POST['gender'];
+
+    $result = mysql_query("select * from user_form where email = '$email' && password = '$pword'")
+                    or die("Failed to query database ".mysql_error());
+    $row = mysql_fetch_array($result);
+    if ($row['email'] == $email) {
+        echo "Email is already used! Use another email to register";
+    } else {
+        
+    }
+    
+    
+
+    // $select = " SELECT * FROM `user_form` WHERE email = '$email'  && password = '$pword' ";
+    // $result = mysqli_query($conn, $select);
+
+    if(mysqli_num_rows($result) > 0) {
+
+        $error[] = 'Account already exists!';
+
+    } else{
+
+        if($pword != $cpword){
+            $error[] = 'Password does not match!';
+        } else{
+            $insert = "INSERT INTO member_form(firstName, lastName, email, password, gender) VALUES('$fName', '$lName', '$email', '$pword', '$gender')";
+            mysqli_query($conn, $insert);
+            header('location: Login.php');
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,39 +54,8 @@
 </head>
 <body>
 
-<?php
-
-require('config.php');
-
-if(isset($_REQUEST['fName'])){
-
-    $fName = stripslashes($_REQUEST['fName']);
-    $fName = mysqli_real_escape_string($con,$fName );
-    $lName = stripslashes($_REQUEST['lName']);
-    $lName = mysqli_real_escape_string($con, $lName);
-    $email = stripslashes($_REQUEST['email']);
-    $email = mysqli_real_escape_string($con, $email);
-    $pword = stripslashes($_REQUEST['password']);
-    $pword = mysqli_real_escape_string($con,$pword);
-    $cpword = stripslashes($_REQUEST['cPassword']);
-    $cpword = mysqli_real_escape_string($con,$cpword);
-    $gender = stripslashes($_REQUEST['gender']);
-    $gender = mysqli_real_escape_string($con,$gender);
-
-    $accDup = " SELECT * FROM `localiusers` WHERE email = '$email'";
-    $result = mysqli_query($con, $accDup);
-
-    if(mysqli_num_rows($result) > 0) { $error[] = 'Account already exists!'; }
-    else if($pword != $cpword) {$error[] = 'Passwords do not match!'; } 
-    else{
-    $sql = "INSERT INTO `localiusers` (`FirstName`, `LastName`, `Email`, `Pass`, `Gender`) VALUES('$fName', '$lName', '$email', '$pword', '$gender')";
-    $con = mysqli_connect($servername, $username, $password, "member_db");
-    if($con->query($sql)){}
-    }
-}
-?>
 <div class="form-container">
-    <form action="" class="form" method="post">
+    <form action="" method="post">
         <h3> Registration </h3>
         <?php
         if(isset($error)){
@@ -50,18 +63,16 @@ if(isset($_REQUEST['fName'])){
                 echo '<span class="error-msg">' .$error. ' </span>';
             };
         };
-        $error = NULL;
         ?>
-
-        <input type="text" name="fName" required="true" placeholder="First name">
-        <input type="text" name="lName" required="true" placeholder="Last name">
-        <input type="email" name="email" required="true" placeholder="E-mail address">
-        <input type="password" name="password" required="true" placeholder="Password">
-        <input type="password" name="cPassword" required="true" placeholder="Confirm password">
+        <input type="text" name="fName" required placeholder="First name">
+        <input type="text" name="lName" required placeholder="Last name">
+        <input type="email" name="email" required placeholder="E-mail address">
+        <input type="password" name="password" required placeholder="Password">
+        <input type="password" name="cPassword" required placeholder="Confirm password">
         <select name="gender">
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="none">Prefer not to state</option>
+            <option value="male"> Male</option>
+            <option value="female"> Female</option>
+            <option value="none"> Prefer not to state</option>
         </select>
         <input type="submit" value="Register" class="form-btn">
         <p> Already have an account? <a href="Login.php"> Login Now </a></p>
