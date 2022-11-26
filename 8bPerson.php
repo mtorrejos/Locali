a4850bbb3219d030f76f5cd6a8091c6bbbd2c5c6
mtorrejos <106376979+mtorrejos@users.noreply.github.com>
2022-11-26 16:53:54 +0800

    Add files via upload

---
+++ 8bPerson.php
@@ -0,0 +1,93 @@
+<?php
+	include_once 'topbar.php';
+?>
+
+<!--Sidebar-->
+<div id="aside">
+    <div class="col-lg-4">
+        <div class="mb-4">
+            <div class="section-title mb-0">
+                <h4 class="m-2 text-uppercase font-weight-bold">Latest News</h4>
+            </div>
+            <div class=" border border-top-0 p-3">
+                <div class="d-flex align-items-center bg-white mb-4" style="height: 105px;">
+                    <img class="img-thumbnail h-100" src="img/latest-news.jpg" alt="Photo of public transportation in the Philippines (jeepney and bus)">
+                    <div class="w-100 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
+                        <div class="mb-2">
+                            <a class="badge badge-dark text-uppercase font-weight-semi-bold p-1 mr-2" href="LatestNews.php">News</a>
+                            <a><small>November 16, 2022</small></a>
+                        </div>
+                        <a class="h6 m-0 text-secondary text-uppercase font-weight-bold" href="JeepBus.php">Jeepney, bus groups ask for rush-hour surcharge</a>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center bg-white mb-4" style="height: 105px;">
+                    <img class="img-thumbnail h-100" src="img/latest-movies.jpg" alt="Photo of Mike de Leon's film classic 'Itim'">
+                    <div class="w-100 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
+                        <div class="mb-2">
+                            <a class="badge badge-dark text-uppercase font-weight-semi-bold p-1 mr-2" href="EntertainmentNews.php">Movie</a>
+                            <a><small>November 16, 2022</small></a>
+                        </div>
+                        <a class="h6 m-0 text-secondary text-uppercase font-weight-bold" href="QCinema.php">Local, international highlights at QCinema</a>
+                    </div>		
+                </div>
+                <div class="d-flex align-items-center bg-white mb-3" style="height: 110px;">
+                    <img class="img-thumbnail h-100" src="img/latest-music.jpg" alt="Photo of Beyonce performing">
+                    <div class="w-100 h-110 px-3 d-flex flex-column justify-content-center border border-left-0">
+                        <div class="mb-2">
+                            <a class="badge badge-dark text-uppercase font-weight-semi-bold p-1 mr-2" href="EntertainmentNews.php">Music</a>
+                            <a><small>November 16, 2022</small></a>
+                        </div>
+                        <a class="h6 m-0 text-secondary text-uppercase font-weight-bold" href="Beyonce.php">Beyonce leads Grammy nominees with 9, ahead of Kendrick Lamar and Adele</a>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<!-- News Article -->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="position-relative mb-3">
+                        <img class="img-fluid w-100" src="img/8b-baby.jpg" alt="Photo of the 8th billionth baby in the arms of her mother" style="object-fit: cover;">
+                        <div class="bg-white border border-top-0 p-4">
+                            <div class="mb-3">
+                                <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2"
+                                    href="TopNews.php">News</a>
+                                <a>November 16, 2022 | The Manila Times </a>
+                            </div>
+                            <h1 class="mb-3 text-secondary text-uppercase font-weight-bold">Filipino baby is 8 billionth person in the world, says PH population commission </h1>
+                            <p> According to a post on the Commission on Population and Development's Facebook page published early on Wednesday, a newborn born in the Philippines is the 8th billionth individual in the world.<p>
+
+                            <p>On November 15, 2022, Vinice Mabansag of Del Pan in Tondo, Manila, was born. <p>
+
+							<p> To commemorate the occasion, the commission presented the woman and her infant a "welcome cake." <p>
+
+							<p>As of the time of publication, there are no additional details. <p>
+
+							<p>When the eighth billionth baby is born, as predicted by the UN on Tuesday, it will be "an occasion to celebrate variety and advancements while contemplating humanity's shared duty for the planet," according to Secretary General Antonio Guterres.  </p>
+						   
+                        </div>
+                    
+                    </div>
+				</div>
+					
+    <div class="container-fluid py-4 px-sm-3 px-md-5" style="background: #111111;">
+        <p class="m-0 text-center">&copy; <a>Locali News</a>. All Rights Reserved. 
+		
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary btn-square back-to-top"><i class="fa fa-arrow-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>				
